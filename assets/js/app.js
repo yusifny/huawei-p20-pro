5ff57e92b21e5b1f80ddb1e73b7f81f3c15aca6b
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-08-13 02:36:33 +0400

    app.js done

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#time').text("15:10");
+    $('#date').text("Thu, Aug 11");
+    $('#hm-btn').click(function(){
+        $('#lock-sc').toggle();
+        $('#disp').toggleClass('disp-lock');
+        new Audio('assets/audio/cs-lock-sound.mp3').play();
+    });
+
+    $('.key').click(function(){
+        let val = $('#query').val();
+        let key = $(this).text();
+        if(key == '='){
+            $('#result').text(eval(val));
+        }if(key == 'c'){
+            $('#query').val('');
+            $('#result').text('');
+        }else{
+            $('#query').val(val + key);
+        }
+    })
+    $('.dark-mode-btn').click(function(){
+        // $('body').toggleClass('dark-mode');
+        $('.disp').toggleClass('disp-dark');
+        $('#query').toggleClass('inp-dark');
+        $('.one').toggleClass('one-dark');
+        $('.two').toggleClass('two-dark');
+        $('.key').toggleClass('key-dark');
+        $('.minus').css('background-color', '#191919 important');
+        $('.plus').toggleClass('key-dark');
+    });
+});
